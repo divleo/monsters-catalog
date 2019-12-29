6f1248a854341391a6a88f1004224411edd0c59e
divleo <11596691+divleo@users.noreply.github.com>
2019-12-29 11:58:30 +0200

    Renew filenames

---
+++ src/components/app/app.component.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import CardList from '../card-list/card-list.component';
+
+import './app.styles.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        const monsters = users.map((user) => {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+          };
+        });
+
+        return monsters;
+      })
+      .then((monsters) => this.setState({ monsters }));
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchField: event.target.value });
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters =
+      searchField.length === 0
+        ? monsters
+        : monsters.filter((monster) => {
+            return monster.name
+              .toLowerCase()
+              .includes(searchField.toLowerCase());
+          });
+
+    return (
+      <div className="app">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={this.onSearchChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
